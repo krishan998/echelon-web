046faab03f19a30dfc3e204a0137a81f4451b338
Shubh Raisinghani <raisinghanishubh@gmail.com>
2025-02-02 14:42:29 +0530

    fix: space bw logo and title

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -10,7 +10,7 @@ export function Navbar() {
           <div className="flex items-center">
             <Link to="/" className="flex items-center">
             <img src={logo} alt="Nexbit Logo" className="h-8 w-8" />
-              <span className="ml-2 text-xl font-bold text-black">Nexbit</span>
+              <span className="ml-1 text-xl font-bold text-black">Nexbit</span>
             </Link>
             <div className="hidden md:flex ml-10 space-x-8">
               <Link to="/features" className="text-black hover:text-primary-600 transition-colors">
